3ab91de20ef602cbde2817022f5a404cc663b26c
Henry Farr <hfarr@hey.com>
2021-12-19 00:38:17 -0800

    second pass selection

--- views/js/modules/document/controller.mjs
+++ views/js/modules/document/controller.mjs
@@ -34,32 +34,91 @@ export class HTMLController extends Controller {
   }
 
 
+  validNode(node) {
+
+    if (node.innerHTML) {
+      return node.innerHTML.trim() !== ""
+    } else {
+      return node.textContent !== ""
+    }
+
+  }
+
+  patchText(textNode) {
+
+    // Oops! it breaks because the parent element usually has a lot more text.
+    // we still need to recover inner html though
+    const d = document.createElement('div')
+    d.appendChild(textNode.cloneNode())
+    // if textNode.parentElement.childNodes.indexOf(textNode)
+
+    return d.innerHTML.trim().split(/\s+/).join(' ')
+
+    // not me seriously parsing html with regex :S
+    // hf.innerHTML.trim().split(/\s*<[^>]+>\s*/)
+  }
+
+
+
+  /**
+   * Gets the character (or "atomic") offset of a point from the start of the 
+   * document given by a node and offset into the node.
+   * 
+   * more generally, text offset selected in a range even.
+   * but we work out which text is ignorable
+   * 
+   * @param node 
+   * @param offset 
+   */
+  getTextLength(startNode, node, nodeOffset) {
+
+    if (startNode === node) {
+      // Goal case
+      return nodeOffset;
+    }
+
+    if (startNode.nodeType === Node.TEXT_NODE) {
+      // let huh = this.patchText(startNode)
+      // console.debug("child", huh);
+      return this.patchText(startNode).length;
+    }
+
+    let docRange = document.createRange();
+    // console.log(startNode)
+    docRange.selectNodeContents(startNode);
+    const comparison = docRange.comparePoint(node, nodeOffset) 
+    docRange.detach()
+
+    switch(comparison) {
+      case -1:
+        return 0;
+      case 0:
+      case 1: {
+        // let length = 0
+        // for (const cn of startNode.childNodes) {
+        //   length += this.getTextLength(cn, node, nodeOffset);
+        // }
+        // // console.debug("length:",length)
+        // return length
+
+        // Computes sum of list, + 1 char for each counted space between
+        return [...startNode.childNodes]
+          .filter(n => n !== 0)
+          .map(cn => this.getTextLength(cn, node, nodeOffset))
+          .reduce((p,c) => p + c, 0)
+      }
+    }
+
+
+  }
 
 
   computeDocumentRange(htmlRange) {
-    let docRange = document.createRange()
+    console.debug("Brother may I have some lengths")
+    const startOffset = this.getTextLength(this.editor, htmlRange.startContainer, htmlRange.startOffset)
+    const endOffset = this.getTextLength(this.editor, htmlRange.endContainer, htmlRange.endOffset)
 
-    let summation = -1;
-    let index = 0;
-    summation = 0;
-
-    docRange.selectNode(this.editor.children[index])
-    while ( docRange.comparePoint(htmlRange.startContainer, 0) === 1 ) {  // while the start of the range is after docRange
-      console.debug(`${index}: {${docRange.toString()}}`, docRange)
-      summation += docRange.endOffset;  // start offset is 0 so endOffset represents the text length that's selected.
-      docRange.selectNode(this.editor.children[++index])
-    }
-    let startOffset = summation + htmlRange.startOffset;
-    console.log("Start offset", startOffset)
-    while ( docRange.comparePoint(htmlRange.endContainer, 0) === 1 ) {  // while the end of the range is after docRange
-      console.debug(`{${docRange.toString()}}`)
-      summation += docRange.endOffset;  // start offset is 0 so endOffset represents the text length that's selected.
-      docRange.selectNode(this.editor.children[++index])
-    }
-    console.log("End offset", summation + htmlRange.endOffset)
-
-    docRange.detach()
-    return [startOffset, (summation + htmlRange.endOffset) - startOffset]
+    return [startOffset, endOffset - startOffset]
 
   }
 
