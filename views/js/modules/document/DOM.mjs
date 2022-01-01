de052d2e288a72503e321f7409a44d974648971b
Henry Farr <hfarr@hey.com>
2021-12-31 21:28:56 -0800

    Migrate code to module
    Hook up the original editor.njk file to use the module instead of the
    code embedded in the markup.
    
    In addition to cursor now can pick up selected text

--- views/js/modules/document/DOM.mjs
+++ views/js/modules/document/DOM.mjs
@@ -50,6 +50,191 @@ function empty(node) {
 }
 
 /**
+ * Recursive function to walk a node and compute the "user rendered"
+ * string. The key objective is to compute the text that would be rendered
+ * for nodes that have not necessarily been attached to the DOM, or a subset
+ * of a tree that is attached to the DOM. 
+ * innerText accomplishes the same goal but only for complete subtrees of the
+ * DOM. If for example you only want a subset of nodes, such as those 
+ * representing text selected by the user, you cannot easily deduce the indices
+ * of innerText that would represent the selction, or where the user cursor 
+ * lands.
+ * @param node Current node
+ * @param children String 
+ * @param childrenNodes Original child nodes
+ * @returns 
+ */
+function process(node, children, childrenNodes) {
+
+  // TODO develop abstractions to manage the details of particular nodes, etc.
+
+  if (node.nodeType === Node.COMMENT_NODE) {
+    return ""
+  }
+  if (!node.hasChildNodes()) {
+    // The result is "" for childless elements, text for text nodes
+    return node.textContent
+  }
+
+  const oneOrMoreWhitespace = /[^\P{White_Space}\u{00A0}]+/u
+
+  function zip(l1, l2, f = (a, b) => [a, b]) {
+    const res = []
+    for (let i = 0; i < l1.length && i < l2.length; i++) {
+      res.push(f(l1[i], l2[i]))
+    }
+    return res
+  }
+  // here is where we have room to improve. We want to focus on converting
+  // from browser representation to internal document. Right now this
+  // relies on expectations about browser behavior or even the kinds of HTML
+  // we'll munch on. Might be better served parsing the raw for internal use
+  // so it's independent. That and we don't always need to compute selection
+  // lengths, it's relevant for user operations when something is selected
+  // yes but for many cases its not. We could for example re-build the document
+  // internally on a cut or paste rather than try to mirror actions. 
+  // Im rambling, I would like to stick to using the browser as a controller
+  // and not the source of truth.
+  function betwixt([[c1Text, c1Node], [c2Text, c2Node]]) {
+    if (allWhiteSpace(c1Text)) {
+      return "";
+    }
+
+    if (blockNodes.test(c1Node?.tagName)) {
+      // innerText shows block elements with two spaces. Probably should test the space "between" block elements,
+      // which I think would entail testing c2 and not c1. Or, maybe it doesn't matter since it gets cut?
+      // ehhhh I need to break this up into an abstraction to finesse the details with ease (ironic, I suppose)
+      // In this case the counting is equivalent, but notionally I'm not operating in a "between" sense even
+      // (rather an "after" sense) though that's the context this function, ostensibly, operates under
+      return "\n\n";
+    }
+    if (oneOrMoreWhitespace.test(c1Text.slice(-1)) || oneOrMoreWhitespace.test(c2Text.slice(0, 1))) {
+      return " "
+    }
+    return ""
+  }
+  function allWhiteSpace(string) {
+
+    // string consists of all whitespace that aren't &nbsp;
+    // this doesn't work for new lines
+    return /^[^\P{White_Space}\u{00A0}]*$/u.test(string)
+  }
+
+  function collapseInternalBreaking(string) {
+    return string.replaceAll(/[^\P{White_Space}\u{00A0}]+/gu, ' ')
+  }
+  function trimLeading(string) {
+    return string.replace(/^[^\P{White_Space}\u{00A0}]/u, '')
+  }
+  function trimTrailing(string) {
+    return string.replace(/[^\P{White_Space}\u{00A0}]+$/u, '')
+  }
+
+  // The zip is "eager", it will fill in undefineds for remaining resources
+  let adjacent = zip(children, childrenNodes)
+
+  let paddingBetween = zip([["", undefined], ...adjacent], adjacent).map(betwixt)
+
+  let collapsed = children.map(collapseInternalBreaking)
+    .map((s, i) => i > 0 ? trimLeading(s) : s)
+    .map((s, i) => i < children.length - 1 ? trimTrailing(s) : s)
+
+  let contextRender = zip(paddingBetween, collapsed, (s1, s2) => `${s1}${s2}`)
+
+  let textRender = contextRender.join('')
+
+  return textRender;
+
+}
+
+/**
+ * Perform a right-associative reduction of a DOM node.
+ * Notionally this is equivalent to using reduceRight on a pre-order traversal of the tree.
+ * 
+ * @param f Function to combine the accumulated values with the current value. Arguments are ordered current, accumulated
+ * @param base Base element. In practice there a no "empty trees", rather, 
+ *             a single base element attached to the "right" most leaf. This supports using treeFoldr
+ *             as if the tree were a list. That is to say inputting an "empty tree" won't return the
+ *             base value, instead the results are not well defined. For now this is intended but
+ *             subject to revision in the future.
+ * @param tree Tree on which to operate.
+ * @returns 
+ */
+function treeFoldr(f, base, tree) {
+
+  // if (tree.childNodes.length === 0) {
+  // it probably *should* return base -- this impacts like. All of the clients are impacted.
+  // Nevertheless. It's tricky, this was developed for 'node' specifically, and the leafs of
+  // DOM nodes are textNodes generally. It's not always clear what to do for a child case.
+  // That is a bit of an excuse.
+  // TODO upddate this to return base, as one would generally expect, or put thought into
+  // why it should apply 'tree' to base instead.
+  // User responsbility to work out what the 'base' case is, as otherwise it should just be
+  // empty lists most of the time, for treeFoldr.
+  // Whats hard is if the node has no children usually you want it processed the same way,
+  // or rather, unlike lists where the base case is the empty list, we don't support a concept
+  // of an "Empty tree" as this was defined with Nodes in mind. But we can rework it to support
+  // a 'leaf' notion as something without a value, then it can handle e.g text nodes fine.
+  // Another thought, in a tree, there can be many bases, but in a list there is only one base,
+  // which disrupts our notion of how to handle that. It seems to make the most sense to use
+  // a function to combine the current value with whatever the "base" is, which allows us
+  // to preserve the notion of a "single base" and maintain the idea that this is effectively
+  // a reduceRight on an array. Except the array is specified by a DOM tree.
+  // return base
+  // return f(tree, base)
+  // }
+
+  const listapply = (treeList) => {
+    return treeList.reduceRight((res, item) => treeFoldr(f, res, item), base)
+  }
+
+  return f(tree, listapply([...tree.childNodes]))
+}
+
+
+function _clonc(predicate, f, base) {
+
+  return function (n, prev) {
+    if (predicate(n)) {
+      return f(n, base)
+    }
+    return f(n, prev)
+  }
+}
+
+function pruneDown(root, target) {
+  // console.log(treeFoldr(_clonc( x => x === target, treeTraverseClone(), []), [], root)[0])
+  // console.log(treeFoldr(clonc( x => x === target, treeTraverseClone(), []), [], root)[0])
+
+  return treeFoldr(_clonc(x => x === target, treeTraverseClone(), []), [], root)[0]
+  // return treeFoldr(clonc( x => x === target, treeTraverseClone(), []), [], root)[0]
+}
+
+function treeTraverseClone() {  //default just clones the tree. okay can't think right now how to approach creating a structure-preserving tree map with a fold-right. I Think it should be do-able
+  const parentMap = new WeakMap()
+
+  function _treeID(node, previous) {
+    // b/c of visit order, the children of the node make up the prefix to the list.
+    // so we don't have to do anything else to preserve the structure other than filter for them
+    // we have the following invariant: if i < j then nodes[i] appears before nodes[j] in the document
+
+    const clone = node.cloneNode()
+    parentMap.set(clone, node.parentElement)
+    // const numChildren = previous.filter( p => p.parentElem === node).length
+    // previous.slice(0, numChildren)
+    // .forEach(n => clone.appendChild(n) )
+
+    // a partition sort of function
+    const [kids, siblings] = previous.reduce(([c, s], n) => (parentMap.get(n) === node ? [[...c, n], s] : [c, [...s, n]]), [[], []]) // has functional programming gone TOO FARR? some would say: not nearly enough!
+
+    kids.forEach(n => clone.appendChild(n))
+
+    return [clone, ...siblings]
+  }
+  return _treeID
+}
+
+/**
  * treeTraverse folds over the tree by applying functions that do an
  * in order traversal. It is built on using treeFoldr, but the difference
  * is the input function operates on the tree and its children that have
@@ -68,7 +253,11 @@ function empty(node) {
  *          results of that nodes children, and the original children nodes
  */
 function treeTraverse(f, tree) {
-
+  // TODO should work on trees generally, and write an extension for Node
+  // and any concrete tree representations (e.g an object) that implements
+  // a "treeTraverse" interface. Which, for us, probably just means providing
+  // functions that treeTraversecan call to access the Node, the Children,
+  // etc. without making assumptions, which it does for node traversal.
 
   function unzip(pairs) {
     return pairs.reduce(([accFst, accSnd], [fst, snd]) => [[...accFst, fst], [...accSnd, snd]], [[], []])
@@ -83,10 +272,28 @@ function treeTraverse(f, tree) {
 
   }
 
-  return unzip(_treeFoldr(_combine, [], tree))[1][0]
+  return unzip(treeFoldr(_combine, [], tree))[1][0]
 
 }
 
+// Public
+
+export function renderedText(rootElement, selectedNode) {
+  let pruned = pruneDown(rootElement, selectedNode)
+  // let finalChildren
+  // function procWrap(node, children, nodes) {
+  //   // Light wrapper to extract the final result of the children before they're
+  //   // concatenated in the result.
+  //   let result = process(node, children, nodes)
+  //   finalChildren = children;
+
+  //   return result
+  // }
+
+  // const res = treeTraverse(procWrap, pruned)
+  // return res.slice(0, -selectedNode.textContent.length)
+  return treeTraverse(process, pruned)
+}
 
 /**
  * Normalizes HTML
