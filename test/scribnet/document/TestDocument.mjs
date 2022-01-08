9f21c6d9e65a19884d88901a4294d1d2c1aadd6c
Henry Farr <hfarr@hey.com>
2022-01-08 15:09:03 -0800

    Add test for split, refactor. Syntax formatting.

--- test/scribnet/document/TestDocument.mjs
+++ test/scribnet/document/TestDocument.mjs
@@ -11,38 +11,38 @@ function arrEq() {
 }
 
 
-describe('hooks', function() {
-  before(function() {
+describe('hooks', function () {
+  before(function () {
     const { Segment, applyTag } = expose
 
     const basicSegment = Segment.taggedSegment('p', ...['A test segment'])
 
   })
-  after(function() {
+  after(function () {
 
   })
 })
 
-describe('Segment', function() {
+describe('Segment', function () {
   const { Segment } = expose
   const basicSegment = Segment.taggedSegment(['p'], 'A test segment')
   const segmentEmStrong = Segment.taggedSegment(['p', 'em', 'strong'], ...['A test segment'])
 
-  describe('Segment.taggedSegment', function() {
+  describe('Segment.taggedSegment', function () {
 
   })
 
-  describe('applyTags', function() {
-    
-    it('should add the tag if not present', function() {
+  describe('applyTags', function () {
+
+    it('should add the tag if not present', function () {
       assert.strictEqual(basicSegment.applyTags(['strong']).tags.includes('strong'), true)
     })
 
-    it('has the same characters', function() {
+    it('has the same characters', function () {
       assert.deepStrictEqual(basicSegment.applyTags(['em', 'strong']).characters, basicSegment.characters)
     })
 
-    it('should not add the tag if already present', function() {
+    it('should not add the tag if already present', function () {
       // const basicSegment = Segment.taggedSegment('p', ...['A test segment'])
       // Not exactly testing equality of the array, but if the lengths were different that would signal. This is bad testing hygene, hmm?
       // Keeping length and not using strictDeepEqual because we do not require anything about the order of the tags,
@@ -54,25 +54,29 @@ describe('Segment', function() {
     })
   })
 
-  describe('replaceTags', function() {
+  describe('replaceTags', function () {
     const replaced = basicSegment.replaceTags(['h1', 'em'])
-    it ('should have the new tags', function() {
+    it('should have the new tags', function () {
       assert(replaced.tags.includes('h1'))
       assert(replaced.tags.includes('em'))
     })
-    it ('should remove old tags', function() {
+    it('should remove old tags', function () {
       assert(!replaced.tags.includes('p'))
     })
   })
 
+  // describe('split', function() {
 
-  describe('<Segment instance>.applyTag', function() {
+  //   it('should ')
+  // })
+
+  describe('<Segment instance>.applyTag', function () {
   })
 })
 
-describe('ListSegment', function() {
+describe('ListSegment', function () {
   const { ListSegment, Segment } = expose
-  const listSeg = ListSegment.from(
+  const listSeg1 = ListSegment.from(
     Segment.taggedSegment(['h1'], 'Titular Segment'),
     Segment.taggedSegment(['p'], 'body text. Wonderful'),
     Segment.taggedSegment(['p', 'em'], 'ly emphasized'),
@@ -80,21 +84,37 @@ describe('ListSegment', function() {
     Segment.taggedSegment(['p', 'strong'], 'go'),
     Segment.taggedSegment(['p'], ' away.'),
   )
-  it('has the characters of its components combined', function() {
+
+  const listSeg2 = ListSegment.from(
+    Segment.taggedSegment(['h1'], 'Test Content'),
+    Segment.taggedSegment(['p'], 'Bare text then'),
+    Segment.taggedSegment(['p', 'em'], 'some emphasis'),
+    Segment.taggedSegment(['p'], 'BUT not here'),
+  )
+  it('has the characters of its components combined', function () {
     assert.deepStrictEqual(
-      listSeg.characters.join(''),
+      listSeg1.characters.join(''),
       "Titular Segmentbody text. Wonderfully emphasized! Boldly go away."
     )
   })
 
-  describe('applyTags', function() {
-    const listSeg = ListSegment.from(
-      Segment.taggedSegment(['h1'], 'Test Content'),
-      Segment.taggedSegment(['p'], 'Bare text then'),
-      Segment.taggedSegment(['p', 'em'], 'some emphasis'),
-      Segment.taggedSegment(['p'], 'BUT not here'),
-    )
-    const boldApply = listSeg.applyTags(['strong'], 31, 42)
+  describe('split', function () {
+    it('has one more segment when split', function () {
+      for (let i = 0; i <= listSeg1.length; i++) {
+        const expected = listSeg1.segments.length + 1
+        const actual = listSeg1.split(i).segments.length
+        assert(actual === expected, `Failed for listSeg1 segment ${i}. Actual length ${actual}, expected ${expected}`)
+      }
+      for (let i = 0; i <= listSeg2.length; i++) {
+        const expected = listSeg2.segments.length + 1
+        const actual = listSeg2.split(i).segments.length
+        assert(actual === expected, `Failed for listSeg2 segment ${i}. Actual length ${actual}, expected ${expected}`)
+      }
+    })
+  })
+
+  describe('applyTags', function () {
+    const boldApply = listSeg2.applyTags(['strong'], 31, 42)
     const boldApplyExpected = ListSegment.from(
       Segment.taggedSegment(['h1'], 'Test Content'),
       Segment.taggedSegment(['p'], 'Bare text then'),
@@ -104,19 +124,12 @@ describe('ListSegment', function() {
       Segment.taggedSegment(['p'], ' not here'),
     )
 
-    it('applies tags correctly', function() {
-      // console.log(boldApply)
+    it('applies tags correctly', function () {
       assert(boldApply.eq(boldApplyExpected))
-      // assert.deepStrictEqual(boldApply[0]?.characters, 'Test Content')
-      // assert.deepStrictEqual(boldApply[1]?.characters, 'Bare text then')
-      // assert.deepStrictEqual(boldApply[2]?.characters, 'some ')
-      // assert.deepStrictEqual(boldApply[3]?.characters, 'emphasis')
-      // assert.deepStrictEqual(boldApply[4]?.characters, 'BUT')
-      // assert.deepStrictEqual(boldApply[5]?.characters, ' not here')
-    }) 
+    })
   })
 
-  describe('_locate', function() {
+  describe('_locate', function () {
 
   })
 })
