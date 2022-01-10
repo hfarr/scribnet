713e353d7ba8790645fcd6fc7a201b9d179757cb
Henry Farr <hfarr@hey.com>
2022-01-09 22:48:19 -0800

    First pass at HTML rendering

--- views/js/scribnet/document/Renderer.mjs
+++ views/js/scribnet/document/Renderer.mjs
@@ -61,6 +61,51 @@ class HTMLRenderer extends Renderer {
   get wrapperStyling() {  // hmmmmmmmmmmmmmmmmmmmm
     return "white-space: pre-wrap;" // yeah. I think. In a shadow-dom world we'd just Not and leave it to the component, but I havent component'd renders yet.
   }
+
+
+
+  toHTML() {
+    // Not the most elegant 'prettyprinter'
+    if (!this.canShowHTML()) return ""
+    let result = ""
+    let currentBlock = undefined
+    // let inlineTags
+    let inlineContext = ""
+    const blocks = ["h1", "h2", "h3", "p"].map(s=>s.toUpperCase())
+
+    // Extract out a tool for going from listLike->treeLike? that is, from a linear input, lift it to a tree
+    // in the way we have tools for collapsing a tree down to a list
+    const isBlock = segment => blocks.some(blockTag => segment.tags.includes(blockTag))
+    const newBlock = () => currentBlock === undefined
+
+    // const closeblock = ()=> `<${currentBlock}>${inlineContext.replaceAll('\n','<br>')}</${currentBlock}>`
+    // I guess we won't use replaceAll since I would need the compiler to target es2021? Would prefer to keep it compatible-ish
+    // const renderBlock = () => `<${currentBlock.toLowerCase()}>${inlineContext.replace(/\n/g, '<br>')}</${currentBlock.toLowerCase()}>`
+
+    const wrapOne = tag => (_, value) => `<${tag.toLowerCase()}>${value}</${tag.toLowerCase()}>`
+    const renderBlock = () => wrapOne(currentBlock)`${inlineContext.slice(0,-1).replace(/\n/g, '<br>')}`  // cut out last \n
+    const wrap = (tags, content) => tags.length === 0 ? content : wrapOne(tags[0])`${wrap(tags.slice(1), content)}`
+
+    const closeBlock = () => {
+      if (currentBlock !== undefined) {
+        result += renderBlock()
+        currentBlock = undefined
+        inlineContext = ""
+      }
+    }
+
+    for (const segment of this.editDocument.text.segments) {
+      if (newBlock()) {
+        currentBlock = segment.tags.find(t => blocks.includes(t))
+      } else {
+        const inlineTags = segment.tags.filter(t => !blocks.includes(t))
+        inlineContext += wrap(inlineTags, segment.characters.join(''))
+
+        if (segment.characters.at(-1) === "\n") closeBlock()
+      }
+    }
+    return result
+  }
 }
 
 // big custom component potential y'know
@@ -70,7 +115,7 @@ class EditRenderer extends Renderer {
   }
 
   get wrapperStyling() {
-    return "white-space: pre-wrap;" 
+    return "white-space: pre-wrap;"
   }
 
   indicator(text) {
