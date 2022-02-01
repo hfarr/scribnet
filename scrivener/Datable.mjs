817fab925bb2e075d65659aef9c8d43a8365e4ff
Henry Farr <hfarr@hey.com>
2022-01-31 23:04:33 -0800

    Implement Datable mixin pattern
    Early draft

--- scrivener/Datable.mjs
+++ scrivener/Datable.mjs
@@ -12,7 +12,7 @@ class Database {
   // filesystem DB for now. And largely in-memory I'll note.
 
   static async initFileDB(filename) {
-    const db = new Database() 
+    const db = new Database()
     db.filename = filename
     return db.loadDB()
   }
@@ -24,7 +24,7 @@ class Database {
 
     // might be able to do it faster if I do a "seek/scan" type situation but we simply... won't for now.
     // the memory is temporary, the performance cost infrequent
-    return fs.readFile(this.filename, { encoding: 'utf16', flag: 'a+' } )
+    return fs.readFile(this.filename, { encoding: 'utf16', flag: 'a+' })
       .then(doLoad) // 'this' within a callback here, could be pain, so I bind it outside just to be safe. I might inline the lambda if I go back and read through the promise api again
 
     // below: wanted to "seek" over the file encoded char by encoded char.
@@ -51,7 +51,7 @@ class Database {
     const datables = string.split('\n')
 
     for (const datum of datables) {
-      const {class: className, data: data} = matched
+      const { class: className, data: data } = matched
 
       const position = matched.index + className.length // position of the data, not its meta data
       this.positionTable.set(position, data)
@@ -76,7 +76,7 @@ class Database {
   save(content /* Datable */) {
     if (!(content instanceof Datable)) {
       throw new Error("Could not save object (Not serializeable):", content)
-    } 
+    }
 
     const promise = fs.open(this.filename)
       .then(file => {
@@ -91,10 +91,11 @@ class Database {
 }
 
 const isDatable = obj => {
+  return false
   if (datable in obj) {
     return true
   }
-  
+
   if (Object.getPrototypeOf(obj) !== Object.prototype) {
     return isDatable(Object.getPrototypeOf(obj))
   }
@@ -102,80 +103,118 @@ const isDatable = obj => {
   return false
 }
 
+class Dataccess {
+  constructor() {
+    this.constructors = {}
+  }
+
+  register(constructorFunc) {
+    const consProto = Object.getPrototypeOf(constructorFunc.prototype)
+    // const newProto = (class extends Datable { static sym = Symbol('datableID') }).prototype
+    const newProto = (class extends Datable { get constructorFunc() { return constructorFunc } }).prototype
+    // Object.setPrototypeOf(newProto, consProto)
+    Object.setPrototypeOf(constructorFunc.prototype, newProto)
+
+
+    // Would maybe like to identify by the symbol. Thing is I don't know if I can reproducibly
+    // serialize the symbols. The trick of this is multiple 'constructors' can have the same name!
+    // we hope not! I can detect that error, but I'm forebearing on that code for now. I'd rather
+    // work out a serialization scheme that can robustly handle same-named constructors...
+    // or maybe make it an error to call register more than once per Dataccess :S and enforce
+    // one-name-one-constructor then. Hmm.
+    // TODO handle the above situation.
+    this.constructors[constructorFunc.name] = constructorFunc
+  }
+
+  loadInstance(stringData) {
+    const { class: className } = JSON.parse(stringData)  // TODO unsafe, be warned. Or, verify safety. Strings are dangerous.
+
+    if (!(className in this.constructors)) {  
+      throw new (class DeserializationError extends Error {})(`Cannot load instance of '${className}', the class is not registered.`)
+    }
+    // Create the object with the appropriate prototype (instance of a class)
+    const obj = Object.create(this.constructors[className].prototype)
+    obj.deserialize(stringData)
+
+    return obj
+  }
+}
+
 
 class Datable {
 
-  constructor() { 
-    // this[dataccess] = {
-    //   'serialize': () => {
-    //     return 
-    //   },
-    //   'deserialize': () => {
+  constructor() { // constructor is not called. 'Datable' in concept exists only to conveniently name the prototype
 
-    //   }
-    // }
   }
 
-  static constructors = {}
-  static register(constructor) {
-    // if (!(datable in this)) this[datable] = { constructors: {} }
-    // this[datable].constructors[constructor.name] = constructor
-    this.constructors[constructor.name] = constructor
-  }
+  // Not likely much point to this since we don't actually.. have a Datable class! we can't reference it from the
+  // instances properly since reading the 'constructor' property will return the client class.
+  // if we're in a deserialization context it is client responsibility to check the data they're reading is what
+  // they need.
+  // I might keep this around. hmm. maybe DataAccess should own this since it has the knowledge of the constructors.
+  // static deserializeFrom() {
 
-  // rigid definitions. Only JSON, in future can consider other possibilities
-  // Yeah, I expect these to change. For now I't convenient to define one location.
-  // also thinking, I am going to build in persistence to this class too, as another
-  // stop gap. for that I'll have save and load.
-  // static? mmm, guess it depends. As an inherited feature, we are delcaring what can be serialized.
-  // as a static, we could say, well, we can serialize whatever you hand me, and its not strictly
-  // a property of the object. but it's also decoupled
-  // what I might end up with is using the dataccess symbol. so you can make any class
-  // serializable without the classes own knowledge or participation, since it cross cuts.
-  // sticking to inheritance for now even though I think that's better suited for domain tasks
-  // serialize() {
-  static serialize(obj) {
-    // for (const name of Object.getOwnPropertyNames(this)) {
-    // }
-    // mm hmm. No fine tuning today. Serialization is all properties. Refined access ,refined serialization- will be
-    // a responsibility of scopes, or another "Datable" style subclassification. Properties that are hidden from
-    // some but not others. Hmm hmm. Do more, revisit later. Getting distracted.
-    // return JSON.stringify( { name: this.constructor.name, data: this } )
+  // }
 
-    if (isDatable(obj)) {
+  // eh. Doesn't seem useful for deserialization.
+  get constructorFunc() {}
 
-      const data = {}
-      for (const name of Object.getOwnPropertyNames(obj)) {
-        if (isDatable(obj[name]))
-        data[name] = obj[name]
-      }
+  serialize() {
 
-      // if the object does not yet have an id, or other attributes that a Datable ought, then we can
-      // do that work here. It would be... perhaps... simpler to hook the constructor.
-      // e.g constructHandler['construct'] = function(target, args, newTarget) { console.log("making", newTarget); return new target(args) }
-      // but!!! then we have to sneakyswap the constructor. like. update the prototype. Eugh!
-      return JSON.stringify( { name: this.constructor.name, data: this } )
+    const data = {}
+    for (const name of Object.getOwnPropertyNames(this)) {
+      if (isDatable(this[name]))
+        data[name] = 'placeholder'
+      else
+        data[name] = this[name]
+      
     }
-    return undefined
+
+    return JSON.stringify({ class: this.constructor.name, data: data })
   }
 
-  // deserialize(stringData) {
+  // thought: deserialized object, return an existing object if they are the same? (that is, mem location?)
+  // requires ID'ing by instance. could be fruitful. but could also lead to super unexpected results. Let
+  // that behavior fall out through composition of capabilities. I suppose.
   deserialize(stringData) {
     // const constructorFunc = this.constructor()
     // Note bien that this deserialize function is destructive, it replaces state of an object. it doesn't produce
     // a new one. Again we might change that but I don't have the "feel" of this system yet.
-    const { class: className, data} = JSON.parse(stringData)  // TODO unsafe, be warned. Or, verify safety. Strings are dangerous.
+    const { class: className, data } = JSON.parse(stringData)  // TODO unsafe, be warned. Or, verify safety. Strings are dangerous.
 
-    // for (const name of Object.getOwnPropertyNames(this)) {
-    //   this[name] = obj[name]
-    // }
-
-    // for now, we mandate all Datable things have empty constructors. we can use some metaprogramming to rewire that in the future, support more styles.
-    const obj = Datable.constructors[className]()
-    // may need to use getPropertyDescriptors in case of privacy or summat
-    for (const name of Object.getOwnPropertyNames(obj)) {
-      obj[name] = data[name]
+    // would be okay if className is a subclass of this.constructor
+    if (className !== this.constructor.name) {  
+      throw new (class DeserializationError extends Error {})(`Cannot create ${this.constructor.name} from '${className}'`)
     }
-    return obj
+    // Create the object with the appropriate prototype (instance of a class)
+    // const obj = Object.create(this.constructors[className].prototype)
+
+    // may need to use getPropertyDescriptors in case of privacy or summat
+    // for (const name of Object.getOwnPropertyNames(obj)) {
+    for (const prop in data)
+      this[prop] = data[prop]
+
+    // return obj
   }
 }
+
+
+class Document {
+  constructor() {
+    this.title = "My post"
+    this.content = "my words"
+  }
+
+  publish() {
+    console.log("nyoooom", this.title)
+  }
+}
+
+let dacc = new Dataccess()
+dacc.register(Document)
+
+let doc = new Document()
+
+// console.log(doc.serialize())
+let hoc = dacc.loadInstance(doc.serialize())
+console.log("done")
