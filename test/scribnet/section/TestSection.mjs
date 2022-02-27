a3bacea0747170990358866371c656298b675090
Henry Farr <hfarr@hey.com>
2022-02-27 11:00:49 -0800

    Format & add tests to verify non mutation

--- test/scribnet/section/TestSection.mjs
+++ test/scribnet/section/TestSection.mjs
@@ -21,41 +21,55 @@ describe(`${MODULE} module`, function () {
   const basicSection = Section.from(...listSections)
 
   describe('Section', function () {
-    it('equals itself', function() {
+    it('equals itself', function () {
       assert(basicAtomicSection.eq(basicAtomicSection))
     })
 
-    it('Has the right "atoms"', function() {
+    it('Has the right "atoms"', function () {
       assert.deepStrictEqual(basicAtomicSection.atoms.join(''), testString1)
     })
 
-    it('is equal to its split', function() {
-      for(let i = 0; i < basicAtomicSection.length; i++) {
+    it('is equal to its split', function () {
+      for (let i = 0; i < basicAtomicSection.length; i++) {
         assert(basicAtomicSection.split(i).eq(basicAtomicSection))
       }
     })
 
-    it('deletes correctly', function() {
-      const result = basicAtomicSection.delete(2,7)
-      const expected = AtomicSection.from(..."A section")
-      assert(result.eq(expected))
+    describe('delete', function () {
+      it('deletes correctly', function () {
+        const result = basicAtomicSection.delete(2, 7)
+        const expected = AtomicSection.from(..."A section")
+        assert(result.eq(expected))
+      })
+      it('does not mutate original', function () {
+        basicAtomicSection.delete(2, 7)
+        assert(basicAtomicSection.eq(AtomicSection.from(...testString1)))
+      })
+
     })
 
-    it('inserts correctly', function() {
-      const result = basicAtomicSection.insert(2, "new ")
-      const expected = AtomicSection.from(..."A new test section")
-      assert(result.eq(expected))
+    describe('insert', function () {
+      it('inserts correctly', function () {
+        const result = basicAtomicSection.insert(2, "new ")
+        const expected = AtomicSection.from(..."A new test section")
+        assert(result.eq(expected))
+      })
+      it('does not mutate original', function () {
+        basicAtomicSection.insert(2, "new ")
+        assert(basicAtomicSection.eq(AtomicSection.from(...testString1)))
+      })
     })
+
   })
 
 
   describe('AtomicSection', function () {
 
-    it('equals itself', function() {
+    it('equals itself', function () {
       assert(basicSection.eq(basicSection))
     })
-    it('is equal to its split', function() {
-      for(let i = 0; i < basicSection.length; i++) {
+    it('is equal to its split', function () {
+      for (let i = 0; i < basicSection.length; i++) {
         assert(basicSection.split(i).eq(basicSection))
       }
     })
