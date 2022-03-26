4b1b79d2c54457d5cab04db46b9507b30739ed04
Henry Farr <hfarr@hey.com>
2022-03-26 10:07:11 -0700

    Nest test under 'it' declaration

--- test/scribnet/section/TestContext.mjs
+++ test/scribnet/section/TestContext.mjs
@@ -99,9 +99,12 @@ describe('Context', function() {
     })
 
     describe('updateAttributes', function() {
-      const result1 = testContext1.updateAttributes({ blockTag: 'pre', indentDelta: 2 })
-      assert.strictEqual(result1.block, 'pre')
-      assert.strictEqual(result1.indentation, testContext1.indentation + 2)
+      it ('updates attributes', function () {
+        const result1 = testContext1.updateAttributes({ blockTag: 'pre', indentDelta: 2 })
+        assert.strictEqual(result1.block, 'pre')
+        assert.strictEqual(result1.indentation, testContext1.indentation + 2)
+
+      })
     })
 
   })
