120ff796ddce87e1d364251abeab4c4e7508c3e0
Henry Farr <hfarr@hey.com>
2022-03-26 13:19:33 -0700

    Add test for doc parsing

--- test/scribnet/section/TestContext.mjs
+++ test/scribnet/section/TestContext.mjs
@@ -199,6 +199,19 @@ describe('Context', function() {
       console.log(tagged)
     })
 
+    describe('parseSerialDoc', function () {
+      it('parses JSON serialized Doc into correct Section', function () {
+
+        const serialized = "{\"subPieces\":[{\"subPieces\":[{\"subPieces\":[\"A\",\"a\",\"a\",\"a\",\"a\",\"B\",\"b\",\"b\",\"b\",\"b\"],\"tags\":[]}],\"blockTag\":\"h1\",\"indentationAmount\":0,\"_length\":10,\"_bLength\":11},{\"subPieces\":[{\"subPieces\":[\"C\",\"c\",\"c\",\"c\",\"c\",\"D\",\"d\",\"d\",\"d\",\"d\"],\"tags\":[]}],\"blockTag\":\"p\",\"indentationAmount\":1,\"_length\":10},{\"subPieces\":[{\"subPieces\":[\"E\",\"e\",\"e\",\"e\",\"e\",\"F\",\"f\",\"f\",\"f\",\"f\",\"G\",\"g\",\"g\",\"g\",\"g\"],\"tags\":[]}],\"blockTag\":\"p\",\"indentationAmount\":0,\"_length\":15},{\"subPieces\":[],\"blockTag\":\"p\",\"indentationAmount\":0,\"_length\":0},{\"subPieces\":[{\"subPieces\":[\"H\",\"h\",\"h\",\"h\",\"h\",\"I\",\"i\",\"i\",\"i\",\"🌐\",\"J\",\"j\",\"j\",\"j\",\"j\"],\"tags\":[]}],\"blockTag\":\"h2\",\"indentationAmount\":0,\"_length\":15},{\"subPieces\":[{\"subPieces\":[\"K\",\"k\",\"k\",\"k\",\"k\",\"L\",\"l\",\"l\",\"l\",\"l\"],\"tags\":[]}],\"blockTag\":\"p\",\"indentationAmount\":0,\"_length\":10}],\"_length\":60}"
+
+        const result = Doc.parseSerialDoc(serialized)
+        const expectedEq = testDocAlpha
+
+        assert(result.eq(expectedEq))
+
+      })
+    })
+
     describe('write', function() {
       it('permits writing to an empty doc', function () {
         const newDoc = new Doc()
