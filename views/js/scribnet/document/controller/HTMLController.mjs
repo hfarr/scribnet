de55c5a7501708c3196d7649db6693e16c30c228
Henry Farr <hfarr@hey.com>
2022-03-26 20:35:17 -0700

    Add notes for the SM DSL

--- views/js/scribnet/document/controller/HTMLController.mjs
+++ views/js/scribnet/document/controller/HTMLController.mjs
@@ -19,6 +19,15 @@ function configureQuestionMark(sm) {
   sm.setDefaultActionTransition('insert (', '(|')
   sm.setTransition('(|', 'insert ?', '(?|')
   sm.setTransition('(?|', 'insert )', '(?)|')
+  // sm like "sm.init.insert('(').insert('?').insert(')').to('(?)')"
+  // or sm.init.insert('(?)') as an abbreviation, but still same deal: inserting state after state
+
+  // as well, automatically configuring "context" selection
+  // sm.default.select('(|')       same as "default select, if left char is (, transition to (| state"
+  // sm.default.select('(?')
+  // sm.default.select('(?)')
+  // sm.default.select('^|')        note above in startStarSpace. ^ takes the place of "beginning of Context".
+  //        pipes meaning where the cursor is in relation to the rest of the line. It's a "state machine" language that expands on regular expressions (another statemachine language if you think about it)
 
 }
 
