36d3732574191d908d2cf622ef9a8cd837c4b73a
Henry Farr <hfarr@hey.com>
2022-04-01 13:06:43 -0700

    Implement Parser for Doc

---
+++ views/js/parser/DocParser.mjs
@@ -0,0 +1,68 @@
+import { Parser } from "./Parser.mjs";
+
+import Doc from "../scribnet/section/Doc.mjs";
+import Context from "../scribnet/section/Context.mjs";
+import Segment from "../scribnet/section/Segment.mjs";
+
+const docTokens = [ /(?<Context>\w+)/, /(?<LAngle><)/, /(?<RAngle>>)/, /'(?<SegText>.*?)'/, /\s*/ ]
+
+class DocParser extends Parser {
+
+  static tokenREs = [ /(?<Tag>\w+)/, /(?<LAngle><)/, /(?<RAngle>>)/, /'(?<SegText>.*?)'/, /\s*/ ] 
+
+  parse() {
+
+    return this.doc() 
+
+  }
+
+  doc() {
+    
+    const contexts = []
+    
+    while (!this.isAtEnd()) {
+      contexts.push(this.context())
+    }
+    return Doc.from(...contexts)
+  }
+
+  context() {
+
+    const tok = this.consume('Tag', 'Expect tag at start of context')
+
+    this.consume('LAngle', 'Expect "<"')
+
+    const sections = []
+    while (!this.check('RAngle') && !this.isAtEnd()) {
+      if (this.check('Tag')) {
+        sections.push(this.context())
+      } else if (this.check('SegText')) {
+        sections.push(this.segment())
+      } else {
+
+        this.error(this.peek(), "Expect Context or Segment")
+      }
+      
+    }
+
+    this.consume('RAngle', 'Expect "<"')
+    return Context.createContext(tok.lexeme, ...sections)
+  }
+
+  segment() {
+
+    const segText = this.consume('SegText')
+    // need to expand the language to enable inline tags
+    return Segment.createSegment([], segText.lexeme)
+  }
+}
+
+// -----
+
+const sample = "h1 < 'A List' > ul < li<p<'A'>> li<p<'B'>ul< li<p<'aB'>> li<p<'bB'>> >> li<p<'C'>>> "
+
+const docParser = new DocParser(sample)
+const doc = docParser.parse()
+
+console.debug('testing')
+console.debug(doc.toString())
---
+++ views/js/parser/Parser.mjs
@@ -0,0 +1,134 @@
+
+class Token {
+
+  constructor(type, lexeme, column) {
+    this.type = type
+    this.lexeme = lexeme
+    this.column = column
+  }
+
+}
+
+
+
+
+// e.g Doc microlang
+
+/**
+
+  Doc           ->    Context *
+  Context       ->    Tag < ( Context | Segment ) * >
+  Tag           ->    
+  Segment       ->    ' string '
+  string        ->    .*                  /*
+ 
+ */
+
+
+const tokenize = (string, tokenREs) => {
+  const tokens = []
+  let idx = 0
+
+  while (idx !== string.length) {
+    let errorFlag = true
+
+    for (const re of tokenREs) {
+      const { 0: match, index, groups } = re.exec(string.slice(idx)) ?? {}
+      if (index === 0) {
+
+        if (groups !== undefined) {
+          const type = Object.getOwnPropertyNames(groups)[0]
+          tokens.push(new Token(type, groups[type], idx))
+        }
+        idx += match.length
+
+        errorFlag = false
+        break
+      }
+    }
+
+    if (errorFlag)
+      throw Error("String not accepted by tokens")
+  }
+
+  // end of input token
+  tokens.push(new Token('EOI', ''))
+
+  return tokens
+}
+
+
+class Parser {
+
+  static tokenREs = []
+
+  constructor(string) {
+    this.originalString = string
+    this.current = 0
+    this.tokens = tokenize(string, this.constructor.tokenREs)
+  }
+
+  parse() {
+    // subclass implementation
+    return null
+  }
+
+  match(...types) {
+    for (const type of types) {
+      if (this.check(type)) {
+        this.advance();
+        return true;
+      }
+    }
+    return false;
+  }
+
+  consume(type, message) {
+    if (this.check(type)) return this.advance()
+    this.error(peek(), message)
+  }
+  advance() {
+    if (!this.isAtEnd())
+      this.current++
+    return this.previous()
+  }
+
+  check(type) {
+    if (this.isAtEnd()) return false
+    return this.peek().type === type
+  }
+  isAtEnd() {
+    return this.peek().type === 'EOI'
+  }
+
+  peek() {
+    return this.tokens[this.current]
+  }
+
+  previous() {
+    return this.tokens[this.current - 1]
+  }
+
+  error(token, message) {
+
+    const msg = `Column ${token.column}: ${message}`
+
+    throw Error(msg)
+  }
+
+}
+
+
+class AST {
+
+  constructor() {
+
+  }
+
+}
+
+// ==============
+
+
+
+export { Parser }
