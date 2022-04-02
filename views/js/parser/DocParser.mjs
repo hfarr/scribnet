ddbb9200538ad8cf0b29792a7932e5ae967db408
Henry Farr <hfarr@hey.com>
2022-04-01 17:36:00 -0700

    Implement a "pretty printer" for doc

--- views/js/parser/DocParser.mjs
+++ views/js/parser/DocParser.mjs
@@ -4,7 +4,6 @@ import Doc from "../scribnet/section/Doc.mjs";
 import Context from "../scribnet/section/Context.mjs";
 import Segment from "../scribnet/section/Segment.mjs";
 
-const docTokens = [ /(?<Context>\w+)/, /(?<LAngle><)/, /(?<RAngle>>)/, /'(?<SegText>.*?)'/, /\s*/ ]
 
 class DocParser extends Parser {
 
@@ -57,14 +56,109 @@ class DocParser extends Parser {
   }
 }
 
+class DocPrinter {
+  constructor(topSection) {
+    this.tabDepth = -1
+    this.tabWidth = 2
+    this.topSection = topSection
+  }
+
+  print() {
+    return this.printSection(this.topSection).join('')
+  }
+  get padding() {
+    return Array(this.tabWidth * this.tabDepth).fill(" ").join('')
+  }
+  printSection(section, depth=0) {
+    this.tabDepth = depth
+
+    switch(section.constructor.name) {
+      case 'Doc':
+        return this.printDoc(section, depth)
+      case 'MixedContext':
+        return this.printMixedContext(section, depth)
+      case 'Context':
+        return this.printContext(section, depth)
+      case 'Segment':
+        return this.printSegment(section, depth)
+    }
+  }
+
+  printDoc(section, depth) {
+
+    const lead = `${this.padding}Doc <\n`
+    const follow = `${this.padding}>\n`
+    const childStrings = []
+    for (const subSec of section.subPieces) {
+      childStrings.push(...this.printSection(subSec, depth + 1))
+    }
+
+    return [
+      lead,
+      ...childStrings,
+      follow
+    ]
+
+  }
+  printMixedContext(section, depth) {
+
+    const tag = section.block
+
+    const lead = `${this.padding}${tag} <\n`
+    const follow = `${this.padding}>\n`
+
+    const childStrings = []
+    for (const subSec of section.subPieces) {
+      childStrings.push(...this.printSection(subSec, depth + 1))
+    }
+
+
+    return [
+      lead,
+      ...childStrings,
+      follow
+    ]
+  }
+  printContext(section, depth) {
+
+    const tag = section.block
+
+    const lead = `${this.padding}${tag} < `
+    const follow = '>\n'
+
+    const childStrings = []
+    for (const subSec of section.subPieces) {
+      childStrings.push(...this.printSection(subSec, depth))
+    }
+
+
+    return [
+      lead,
+      ...childStrings,
+      follow
+    ]
+  }
+  printSegment(section, depth) {
+
+    return [ 
+      `'${section.toString()}' `
+    ]
+  }
+}
+
 // -----
 
 const sample = "h1 < 'A List' > ul < li<p<'A'>> li<p<'B'>ul< li<p<'aB'>> li<p<'bB'>> >> li<p<'C'>>> "
 
 const docParser = new DocParser(sample)
 const doc = docParser.parse()
+const docPrinter = new DocPrinter(doc)
 
 console.debug('testing')
 console.debug(doc.toString())
+console.debug(docPrinter.print())
+
+
+export { DocParser, DocPrinter }
 
 export default DocParser
