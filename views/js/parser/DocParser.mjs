90e0da908963d001ec2724f576d95207a3355963
Henry Farr <hfarr@hey.com>
2022-04-23 22:32:08 -0700

    Update to use constants instead of magic strings
    Fix one error message

--- views/js/parser/DocParser.mjs
+++ views/js/parser/DocParser.mjs
@@ -7,7 +7,10 @@ import Segment from "../scribnet/section/Segment.mjs";
 const TAG = 'Tag'
 const RANGLE = 'RAngle'
 const LANGLE = 'LAngle'
+const RPAREN = 'RParen'
+const LPAREN = 'LParen'
 const SEG_TEXT = 'SegText'
+const COMMA = 'Comma'
 const EOI = 'EOI'
 const COMMENT = 'Comment'
 
@@ -46,7 +49,7 @@ class DocParser extends Parser {
       contexts.push(this.context())
     }
     if (hasOptionalTag) {
-      this.consume('RAngle', "Expect '>' at end of Doc tag")
+      this.consume(RANGLE, "Expect '>' at end of Doc tag")
     }
     if (!this.isAtEnd()) {
       this.error(this.peek(), "Expect end of input")
@@ -56,15 +59,15 @@ class DocParser extends Parser {
 
   context() {
 
-    const tok = this.consume('Tag', 'Expect tag at start of context')
+    const tok = this.consume(TAG, 'Expect tag at start of context')
 
-    this.consume('LAngle', 'Expect "<"')
+    this.consume(LANGLE, 'Expect "<"')
 
     const sections = []
-    while (!this.check('RAngle') && !this.isAtEnd()) {
-      if (this.check('Tag')) {
+    while (!this.check(RANGLE) && !this.isAtEnd()) {
+      if (this.check(TAG)) {
         sections.push(this.context())
-      } else if (this.check('SegText') || this.check('LParen')) {
+      } else if (this.check(SEG_TEXT) || this.check(LPAREN)) {
         sections.push(this.segment())
       } else {
 
@@ -73,34 +76,34 @@ class DocParser extends Parser {
       
     }
 
-    this.consume('RAngle', 'Expect "<"')
+    this.consume(RANGLE, 'Expect ">"')
     return Context.createContext(tok.lexeme, ...sections)
   }
 
   segTags() {
-    this.consume('LParen', 'Expect "("')
+    this.consume(LPAREN, 'Expect "("')
 
     const tags = []
 
-    while (!this.check('RParen') && !this.isAtEnd()) {
+    while (!this.check(RPAREN) && !this.isAtEnd()) {
 
-      tags.push(this.consume('Tag', 'Expect tag in segment tags').lexeme)
+      tags.push(this.consume(TAG, 'Expect tag in segment tags').lexeme)
 
-      if (!this.check('RParen')) {
-        this.consume('Comma', "Expect ',' between Segment Tags")
+      if (!this.check(RPAREN)) {
+        this.consume(COMMA, "Expect ',' between Segment Tags")
       }
     }
-    this.consume('RParen', 'Expect ")"')
+    this.consume(RPAREN, 'Expect ")"')
     return tags
   }
 
   segment() {
 
     let tags = []
-    if (this.check('LParen'))
+    if (this.check(LPAREN))
       tags = this.segTags()
 
-    const segText = this.consume('SegText')
+    const segText = this.consume(SEG_TEXT)
     // need to expand the language to enable inline tags
     return Segment.createSegment(tags, segText.lexeme)
   }
